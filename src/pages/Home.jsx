9f7e05ad0b746d803f10a63bf29b6415f3379bc2
Brijendra Tiwari <brijendratiwari993@gmail.com>
2026-03-16 16:04:55 +0530

    progress by 5%

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      {/* Hero Section */}
+      <section className="w-full ">
+
+        
+
+      </section>
     </>
   );
 }
